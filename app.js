045e70745840e0066bc6273742a70dd34098de39
RickWu1 <2502372qaz@gmail.com>
2023-12-11 22:07:39 +0800

    bulid : final code

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require('express')
 const { engine } = require('express-handlebars')
+const express = require('express')
 const app = express()
 const port = 5000
 const restaurant = require('./public/jsons/restaurant.json').results
@@ -11,13 +11,19 @@ app.set('views', './views')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render( 'index', { restaurant })
+  res.redirect('/restaurant')
 })
 
-app.get('/', (req, res) => {
-  res.redirect('/restaurant')
-});
-
+app.get('/restaurant', (req, res) => {
+  const keyword = req.query.keyword?.trim()
+  const macthedrestaurant = keyword ? restaurant.filter((rest) => Object.values(rest).some((property) => {
+    if(typeof property === 'string'){
+      return property.toLowerCase().includes(keyword.toLowerCase())
+    } return false
+  })
+  ) : restaurant
+  res.render('index', {restaurant : macthedrestaurant , keyword})
+})
 app.get('/restaurant/:id', (req, res) => {
   const id = Number(req.params.id)
   const rest = restaurant.find(restaurant => restaurant.id === id )
